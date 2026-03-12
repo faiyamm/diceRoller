52e38a4b04b7499e3655ae802309ff0dc9450abe
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-03-11 22:59:38 -0700

    Initial Commit

---
+++ diceRoller/diceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceRollerApp.swift
+//  diceRoller
+//
+//  Created by Fai on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct diceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
